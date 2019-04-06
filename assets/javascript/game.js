6afdf6da6b2dc3be4f770d92ce1ad807e3e0f01b
Brian Lefkowitz <brianlefkowitz@Brians-MacBook-Pro.local>
2019-04-06 17:11:11 -0400

    finished but could use a little more beautifying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,96 @@
-var wins=0
-var losses=0
-var totalScore=0
-var goalScore = Math.floor(math.random()*200)
-var userScoreArray=[""]
+var wins = 0
+var losses = 0
+var totalScore 
+var goalScore
+var crystalOneValue
+var crystalTwoValue
+var crystalThreeValue
+var crystalFourValue 
 
 $("#crystalOne").on("click", function(){
-    userScoreArray.push(Math.floor(Math.random()*12))
-    console.log(underScoreArray)
-    alert("works")
+    console.log(crystalOneValue);
+    totalScore += crystalOneValue;
+    console.log(totalScore);
+    printValuesToScreen();
+    checkProgress();
 })
 
-$("winner").append(wins)
+$("#crystalTwo").on("click", function(){
+    console.log(crystalTwoValue);
+    totalScore += crystalTwoValue;
+    console.log(totalScore);
+    printValuesToScreen();
+    checkProgress();
+})
 
-$("loser").append(losses)
+$("#crystalThree").on("click", function(){
+    console.log(crystalThreeValue);
+    totalScore += crystalThreeValue;
+    console.log(totalScore);
+    printValuesToScreen();
+    checkProgress();
+})
+
+$("#crystalFour").on("click", function(){
+    console.log(crystalFourValue);
+    totalScore += crystalFourValue;
+    console.log(totalScore);
+    printValuesToScreen();
+    checkProgress();
+})
+
+
+
+function reset(){
+    totalScore=0
+    console.log(totalScore)
+    
+    goalScore = Math.floor(Math.random() * 200 + 50)
+    console.log(goalScore)
+
+    // first number is highest number, second is lowest
+    
+    crystalOneValue = Math.floor(Math.random() * 12 + 1)
+    console.log(crystalOneValue)
+    
+    crystalTwoValue = Math.floor(Math.random() * 12 + 1)
+    console.log(crystalTwoValue)
+    
+    crystalThreeValue = Math.floor(Math.random() * 12 + 1)
+    console.log(crystalThreeValue)
+    
+    crystalFourValue = Math.floor(Math.random() * 12 + 1)
+    console.log(crystalFourValue)
+    
+    printValuesToScreen()
+}
+reset()
+
+function printValuesToScreen(){
+    $("#goal").html(goalScore)
+
+    $("#winner").html("Wins: "+ wins)
+
+    $("#loser").html("Losses: "+ losses)
+
+    $("#userScore").html(totalScore)
+
+}
+
+
+function checkProgress (){
+    if(totalScore === goalScore){
+        alert("Yay You win!!");
+        wins++;
+        reset()
+    }
+    else if (totalScore < goalScore){
+        console.log("keep clicking")
+    }
+    else{
+        alert("Ha-Ha You lose!!");
+        losses++;
+        reset()
+    }
+}
 
